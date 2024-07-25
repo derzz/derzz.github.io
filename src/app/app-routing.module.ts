1886d715a9c1878283b8afcbeab7021751b386fc
Michael <michaelx.10@hotmail.com>
2024-07-25 19:06:22 -0400

    Fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
 
-  {path: ':language?',      component: HomeComponent},
+  {path: '',      component: HomeComponent},
 
   // {path: 'profile'        ,   component: ProfileComponent         , canActivate: [AuthGuard]},
   // {path: 'users'          ,   component: UsersComponent           , canActivate: [AuthGuard]},
